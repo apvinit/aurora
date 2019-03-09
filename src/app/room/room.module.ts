f26e921a509cc626d59331b7b9b3c8f835047351
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-09 12:25:20 +0530

    add material module

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { RoomRoutingModule } from './room-routing.module';
 import { RoomComponent } from './room/room.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [RoomComponent],
   imports: [
     CommonModule,
-    RoomRoutingModule
+    RoomRoutingModule,
+    MaterialModule
   ]
 })
 export class RoomModule { }
