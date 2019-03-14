f0aa768a407f2cd5bb8c4c97d7375e2d3446bda7
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-14 12:39:02 +0530

    Fetched rooms data from api

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/api/room.service';
+import { Observable } from 'rxjs';
+import { Room } from 'src/app/model/room';
 
 @Component({
   selector: 'app-room-list',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomListComponent implements OnInit {
 
-  constructor() { }
+  rooms: Room[];
+  constructor(private roomService: RoomService) { }
 
   ngOnInit() {
+    this.roomService.getRooms().subscribe((rooms: Room[]) => {
+      this.rooms = rooms;
+    });
   }
 
 }
