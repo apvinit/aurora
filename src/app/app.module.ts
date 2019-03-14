477ebc0fb89265fa21f99c3a084e8caed3f8ebb5
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-14 11:37:26 +0530

    Add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { RoomModule } from './room/room.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule,
     RoomModule,
     AppRoutingModule,
