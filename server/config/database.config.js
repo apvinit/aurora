d54747cc44bb5c8c05ced87643cd042ab6bde1a7
h4rdw1r3 <vinaugust6@gmail.com>
2019-03-15 19:13:05 +0530

    Add database config and connect to database

---
+++ server/config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    'mongodb+srv://aurora-app:aurora-app@cluster0-b96vf.mongodb.net/test?retryWrites=true'
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// create
+// create the app
 const app = express();
 
 // parse request of content-type
@@ -10,8 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse request of content-type - application/json
 app.use(bodyParser.json());
 
+// Configuring the database
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((err) => {
+    console.log('Error connecting to the database!,', err);
+  });
+
 // Handle production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   // static folder
   app.use(express.static(__dirname + '/public/'));
 
