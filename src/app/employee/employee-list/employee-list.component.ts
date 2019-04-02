7efe06ab9063f18162d680072b0e111ee583a223
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 15:48:01 +0530

    Fetch and show data
    Get data from the service and update the local properties

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Employee } from 'src/app/model/employee';
+import { MatPaginator } from '@angular/material/paginator';
+import { EmployeeService } from 'src/app/api/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employeeDataSource = new MatTableDataSource<Employee>();
+  columnsToDisplay = ['name', 'department', 'role', 'salary'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
+    this.employeeService.getEmployees().subscribe((employees: Employee[]) => {
+      this.employeeDataSource.data = employees;
+      this.employeeDataSource.paginator = this.paginator;
+    });
   }
-
 }
