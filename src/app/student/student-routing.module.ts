33df9b4fc3aa1b06abd528088bba559ad4a439ec
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 12:15:02 +0530

    Create student component and add material module

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'students',
+    component: StudentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
+import { StudentComponent } from './student.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [StudentComponent],
   imports: [
     CommonModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    MaterialModule
   ]
 })
 export class StudentModule { }
