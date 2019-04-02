4f42a7cae18f85c73555e3c3b4abb2ce7096ad77
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 15:47:20 +0530

    Add employee list compnent to the route

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 const routes: Routes = [
   {
     path: 'employees',
-    component: EmployeeComponent
+    component: EmployeeComponent,
+    children: [
+      {
+        path: '',
+        component: EmployeeListComponent
+      }
+    ]
   }
 ];
 
