c7a5a96188c2dc01c86e26ae4438aab8b6014990
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 12:31:12 +0530

    Add student list component and add to router path

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentComponent } from './student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 const routes: Routes = [
   {
     path: 'students',
-    component: StudentComponent
+    component: StudentComponent,
+    children: [
+      {
+        path: '', component: StudentListComponent
+      }
+    ]
   }
 ];
 
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing.module';
 import { StudentComponent } from './student.component';
 import { MaterialModule } from '../material.module';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
-  declarations: [StudentComponent],
+  declarations: [StudentComponent, StudentListComponent],
   imports: [
     CommonModule,
     StudentRoutingModule,
