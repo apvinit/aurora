1c07bfc87c1a73472c066c1ec1a3d6a463edbcff
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 13:54:30 +0530

    fetch data from the service
    get data from the service and bind to the local property

--- src/app/student/student-list/student-list.component.ts
+++ src/app/student/student-list/student-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from 'src/app/model/student';
+import { MatPaginator } from '@angular/material/paginator';
+import { StudentService } from 'src/app/api/student.service';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  constructor() { }
+  studentDataSource = new MatTableDataSource<Student>();
+  columnsToDisplay = ['name', 'fatherName', 'instituteName', 'roomNo'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private studentService: StudentService) { }
 
   ngOnInit() {
+    this.studentService.getStudents()
+      .subscribe((students: Student[]) => {
+        this.studentDataSource.data = students;
+        this.studentDataSource.paginator = this.paginator;
+      });
   }
 
 }
