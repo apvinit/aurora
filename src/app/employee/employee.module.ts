b85890aedd5fa039c45a21586b30b7c14e447c48
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-02 15:15:30 +0530

    generate employee list component

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee/employee.component';
 import { MaterialModule } from '../material.module';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
-  declarations: [EmployeeComponent],
+  declarations: [EmployeeComponent, EmployeeListComponent],
   imports: [
     CommonModule,
     EmployeeRoutingModule,
